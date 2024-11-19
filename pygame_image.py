d279062362dca96e0c29875106bdf603b8bff88d
C0A23029 <c0a2302985@edu.teu.ac.jp>
2024-11-19 16:01:37 +0900

    練習７　ワープ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bgf_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -17,12 +18,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0]) # screen surfaceに背景画像surfaceを張り付ける
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0]) # screen surfaceに背景画像surfaceを張り付ける
+        screen.blit(bgf_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bgf_img, [-x+4800, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        if tmr > 800:
-            tmr = 0
         clock.tick(200)
 
 
